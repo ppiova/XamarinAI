cf1b00b251445fa348f538c4e9be58905b43b280
Piova <ppiova@gmail.com>
2021-06-02 09:33:16 -0300

    Modify Colours Background Added exported model for TensonFlow and ML for ios

--- AzureCustomVisionDemo/KeysAndUrls.cs
+++ AzureCustomVisionDemo/KeysAndUrls.cs
@@ -4,10 +4,10 @@ namespace AzureCustomVisionDemo
 {
     public class KeysAndUrls
     {
-        #warning You should get all this data from your own project on customvision.ai
+        
         public static string PredictionUrl = "";
         public static string CustomVisionPredictionApiKey = "";
         public static Guid ProjectId = new Guid("");
-        public static string IterationName = "";
+        public static string IterationName = "Iteration4";
     }
 }
